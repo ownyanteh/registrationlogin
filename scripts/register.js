d49fa42c47abda18abfe04f828492158811d1adb
OWN Concepts <mac@OWN-Concepts.local>
2024-09-16 11:37:12 +0000

    first commit

---
+++ scripts/register.js
@@ -0,0 +1,12 @@
+// select form by id
+const registerForm = document.getElementById('register-form');
+// const register = document.querySelector('#register-form');
+
+// handle form submit event
+registerForm.onsubmit = function () {
+// collect username
+// collect email
+// collect password
+// save user information
+// send confirmation email
+}
