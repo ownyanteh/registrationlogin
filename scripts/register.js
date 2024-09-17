7194011ae2eb6a41a0e868c672bbbc7b404730bd
William <ownyanteh@gmail.com>
2024-09-17 13:57:53 +0000

    js push

--- scripts/register.js
+++ scripts/register.js
@@ -1,12 +1,35 @@
 // select form by id
-const registerForm = document.getElementById('register-form');
-// const register = document.querySelector('#register-form');
+// const registerForm = document.querySelector('#register-form');
+const registerForm = document.querySelector('#register-form');
+console.log (registerForm);
 
 // handle form submit event
-registerForm.onsubmit = function () {
-// collect username
-// collect email
-// collect password
-// save user information
-// send confirmation email
+// registerForm.addEventListener ('submit', function () {
+    // collect username
+    // collect email
+    // collect password
+    // save user information
+    // send confirmation email
+    // });
+// registerForm.onsubmit = function () {
+// // collect username
+// // collect email
+// // collect password
+// // save user information
+// // send confirmation email
+// }
+
+// Age Variable
+const age = 44;
+
+// // Square of Age
+function squareAge (age) {
+    const squaredAge = age ** 2;
+return squaredAge;
 }
+
+console.log(squareAge(age));
+
+const squaredAge = squareAge (45)
+console.log (squaredAge);
+
